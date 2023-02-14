65585733f2fa683fcc45215a24a6175ce20247f8
rgsad <rsad3262@gmail.com>
2023-02-14 22:20:10 +0300

    solved the "GSD" task3

--- src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
+++ src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.loops.task3;
 
 public class GreatestCommonDivisor {
     public void printGCD(int first, int second) {
+        int max = first > second ? first : second;
+        int min = first < second ? first : second;
+        int divisor = 1;
+
+        for (int i = max; i > 0; i--) {
+            if (max % i == 0 && min % i == 0) {
+                divisor = i;
+                break;
+            }
+        }
+        System.out.println(divisor);
     }
 }
