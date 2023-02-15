be550ef437d3a0039a5beadfd1aeb2f7a51f9c0a
rgsad <rsad3262@gmail.com>
2023-02-15 22:16:41 +0300

    solved the "FibonacciSeries" task3

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
+        int currentFibonacci = 0;
+        int nextFibonacci = 1;
+
+        for (int i = 1; i <= lastFibonacci; i++) {
+            System.out.println(currentFibonacci);
+            int temp = currentFibonacci;
+            currentFibonacci += nextFibonacci;
+            nextFibonacci = temp;
+        }
     }
 }
